b052e6c01a1817b137a78d51cf02390ee18676fa
Hira Noor <hiranoorbukhari@gmail.com>
2024-08-16 03:35:57 -0700

    MyAssignment

---
+++ assignment.js
@@ -0,0 +1,57 @@
+//Array
+
+//push
+let fruits= ["Apple", "Banana", "Mango", "Grapes"];
+fruits.push("Pear");
+console.log(fruits);
+//Pop
+let persons = ["Sherry", "Hira", "Fariha", "Noor"];
+persons.pop();
+console.log(persons);
+//shift
+const vegetables = ["Carrot", "LadyFinger", "Potato","Peas"];
+vegetables.shift();
+console.log(vegetables);
+//toString
+const ages = ["thirty", "fourty", "fifty", "sixty"];
+ages.toString();
+console.log(ages);
+//unshift
+const fellows = ["Hamsa","Sheeza", "Abeera", "Bushra"];
+fruits.unshift("Hira", "Fariha");
+console.log(fellows);
+//join 
+const school = ["teacher", "Principal", "Students", "Guard"];
+school.join();
+console.log(school);
+//concat
+let arr1 = ["Cecilie", "Lone"];
+let arr2 = ["Emil", "Tobias", "Linus"];
+let children = arr1.concat(arr2);
+console.log(children);
+//delete
+let words=[GoodMorning,GoodAfternoon,GoodEvening,GoodNight];
+words.delete(GoodEvening)
+console.log(words);
+//Reverse
+let classes = [Class1, Class2, Class3,Class4];
+classes.reverse();
+console.log(classes);
+//sort
+const Boys = ["Amir", "Umer", "Naheed", "Majid"];
+Boys.sort();
+console.log(Boys);
+//slice
+let frends=["Javeria", "Fariha", "Hamsa","Nimra"]
+let selected=frends.slice(Javeria,Nimra);
+console.log(selected);
+
+
+
+
+
+
+
+
+
+
